7d1b1247d9ac7bdaf5e47e0539d9403a94e14568
shuaiyuu <zhiaipeter@gmail.com>
2025-05-21 11:44:14 -0500

    Site updated: 2025-05-21 11:44:14

--- js/about_page_script.js
+++ js/about_page_script.js
@@ -1,15 +1,5 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const toggleNewsButton = document.getElementById('toggle-news');
-//     const newsList = document.getElementById('news-list');
-//
-//     toggleNewsButton.addEventListener('click', () => {
-//         if (newsList.style.display === 'none' || !newsList.style.display) {
-//             newsList.style.display = 'block';
-//         } else {
-//             newsList.style.display = 'none';
-//         }
-//     });
-// });
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const toggleNewsButton = document.getElementById('toggle-news');
     const newsList = document.getElementById('news-list');
